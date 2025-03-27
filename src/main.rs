db25480440a5ea8e5497909b2a56474633a5b3a8
yyy <yy-yu21@mails.tsinghua.edu.cn>
2025-03-27 16:52:57 +0800

    init

---
+++ src/main.rs
@@ -0,0 +1,30 @@
+#![no_std]
+#![no_main]
+#![doc = include_str!("../README.md")]
+
+#[macro_use]
+extern crate axlog;
+extern crate alloc;
+
+mod syscall;
+
+use alloc::vec::Vec;
+use starry_core::entry::run_user_app;
+
+#[unsafe(no_mangle)]
+fn main() {
+    let testcases = option_env!("AX_TESTCASES_LIST")
+        .unwrap_or_else(|| "Please specify the testcases list by making user_apps")
+        .split(',')
+        .filter(|&x| !x.is_empty());
+
+    for testcase in testcases {
+        let args = testcase
+            .split_ascii_whitespace()
+            .map(Into::into)
+            .collect::<Vec<_>>();
+
+        let exit_code = run_user_app(&args, &[]);
+        info!("User task {} exited with code: {:?}", testcase, exit_code);
+    }
+}
